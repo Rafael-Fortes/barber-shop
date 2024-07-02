8528f815f94d43bc4219afbb7a97ed9b90139777
Luiz Gustavo Gonçalves Rodrigues <glui3500@gmail.com>
2024-07-02 19:45:35 -0300

    Adicionando try catch nas funções

--- back-end/services/agendamentosService.js
+++ back-end/services/agendamentosService.js
@@ -1,35 +1,58 @@
 const admin = require('../firebaseConfig');
 const db = admin.firestore();
 
+const agendamentosRef = db.collection('agendamentos');
+
 exports.getAgendamentoById = async (id) => {
-  const doc = await db.collection('agendamentos').doc(id).get();
-  console.log(doc)
-  return doc.exists ? doc.data() : null;
+  try {
+    console.log(id);
+    const doc = await agendamentosRef.doc(id).get();
+    console.log(doc)
+    return doc.exists ? doc.data() : null;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 };
 
 exports.createAgendamento = async (data) => {
-  const docRef = await db.collection('agendamentos').add(data);
-  const doc = await docRef.get();
-  return doc.data();
+  try {
+    const docRef = await agendamentosRef.add(data);
+    const doc = await docRef.get();
+    return doc.data();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 };
 
 exports.updateAgendamento = async (id, data) => {
-  const docRef = db.collection('agendamentos').doc(id);
-  const doc = await docRef.get();
-  if (!doc.exists) {
-    return null;
+  try {
+    const docRef = agendamentosRef.doc(id);
+    const doc = await docRef.get();
+    if (!doc.exists) {
+      return null;
+    }
+    await docRef.update(data);
+    const updatedDoc = await docRef.get();
+    return updatedDoc.data();
+  } catch (error) {
+    console.error(error);
+    return error;
   }
-  await docRef.update(data);
-  const updatedDoc = await docRef.get();
-  return updatedDoc.data();
 };
 
 exports.deleteAgendamento = async (id) => {
-  const docRef = db.collection('agendamentos').doc(id);
-  const doc = await docRef.get();
-  if (!doc.exists) {
-    return null;
+  try {
+    const docRef = agendamentosRef.doc(id);
+    const doc = await docRef.get();
+    if (!doc.exists) {
+      return null;
+    }
+    await docRef.delete();
+    return doc.data();
+  } catch (error) {
+    console.error(error);
+    return error;
   }
-  await docRef.delete();
-  return doc.data();
 };
